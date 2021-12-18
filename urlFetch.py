cde2930bfb45d819e043e048e6463218aaed01dd
Ravisha Sharma <ravisha7feb@gmail.com>
2021-12-18 20:31:48 +0530

    add function in python code

--- urlFetch.py
+++ urlFetch.py
@@ -2,7 +2,17 @@ import json
 from urllib.request import urlopen
 url = "https://www.example.com/get-data"
 response = urlopen(url)
-
 json_obj = json.loads(response.read())
-print(json_obj)
+json_obj = list(json_obj) #json_obj should be list/array of dictionaries
 
+#file = open('data.json')
+#data = json.load(file) #data is a list/array of dictionaries
+
+def sortAndPrint(data):
+    data = sorted(data, key = lambda i: i['id'])
+    for entry in data:
+        print(entry['id'], end="  ")
+        print(entry['name'])
+
+
+sortAndPrint(json_obj)
